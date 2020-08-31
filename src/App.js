9675a62ef9a2fbe511187f1dc440cef1107fd020
Barry Yu <doung1118@gmail.com>
2020-08-31 11:55:35 +0800

    3. header.js 裡面coding component header_left , middle , right

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
       {/* Headers */}
       {/* Sidebar */}
       {/* Feed */}
-      {/* widgets */}
+      {/* Widgets */}
 
 
     </div>
--- src/header.js
+++ src/header.js
@@ -1,15 +1,18 @@
 //這邊輸入 rfce   //  可以直接跳出 react的 componet 檔案 javascript 比較快
 
+// 3.git [ 增加div classname="headerleft , mifflr , right "]
 
 
 import React from 'react'
-
-function header() {
+import './Header.css'
+function Header() {
   return (
-    <div>
-
+    <div className="header">
+      <div className="header_left"></div>
+      <div className="header_middle"></div>
+      <div className="header_right"></div>
     </div>
   )
 }
 
-export default header
+export default Header
