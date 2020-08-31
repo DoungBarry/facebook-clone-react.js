7d7d8459883ba3815e35c7d77bddf7879e78f413
Barry Yu <doung1118@gmail.com>
2020-08-31 11:14:40 +0800

    firest project react.js - for faacebook-clone

--- src/App.js
+++ src/App.js
@@ -4,22 +4,33 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    //BEM naming conveting 
+
+    <div className="APP">
+
+      <h1> Let build the facebook clone@!</h1>
     </div>
+
+
+
+    //-----------------------------------刪除 ------------------------
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div >
   );
 }
 
---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAHVLn9OwjrUJJe-u6SLfNyajWfawnFcRo",
+  authDomain: "cool-remote.firebaseapp.com",
+  databaseURL: "https://cool-remote.firebaseio.com",
+  projectId: "cool-remote",
+  storageBucket: "cool-remote.appspot.com",
+  messagingSenderId: "296768918645",
+  appId: "1:296768918645:web:3af5a398e8eee5fc0a943c"
+};
