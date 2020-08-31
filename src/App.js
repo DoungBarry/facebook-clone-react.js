39d3617b252b6d1245efdd002b8c5272a32e55e7
Barry Yu <doung1118@gmail.com>
2020-08-31 12:22:15 +0800

    coding <header /> in app.js ,把Header.js 檔案引入到app.js , header.js 加入圖片fb img

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
@@ -12,6 +13,8 @@ function App() {
     <div className="APP">
 
       <h1> Let build the facebook clone@!</h1>
+      <Header />
+
       {/* Headers */}
       {/* Sidebar */}
       {/* Feed */}
--- src/header.js
+++ src/header.js
@@ -8,7 +8,11 @@ import './Header.css'
 function Header() {
   return (
     <div className="header">
-      <div className="header_left"></div>
+      <div className="header_left">
+
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Facebook_f_logo_%282019%29.svg/1024px-Facebook_f_logo_%282019%29.svg.png" alt=""></img>
+      </div>
+
       <div className="header_middle"></div>
       <div className="header_right"></div>
     </div>
