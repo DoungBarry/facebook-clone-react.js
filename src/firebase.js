d18dbe5f786f84b04e7fc9d6b5973c0b7716ab98
Barry Yu <doung1118@gmail.com>
2020-09-07 17:50:34 +0800

    創建firebase.js ,npm安裝firebase,更新reactDom, 開啟firebase.js 定義ＡＰＰ, 在透過定義資料庫從前面定義ＡＰＰ裡面,去firebase開啟firestore google認證開啟,回到 firebase 設定要export 的項目 auth , provider  ,de

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,6 @@
+import firebase from "firebase"
+
+
 const firebaseConfig = {
   apiKey: "AIzaSyAHVLn9OwjrUJJe-u6SLfNyajWfawnFcRo",
   authDomain: "cool-remote.firebaseapp.com",
@@ -7,3 +10,24 @@ const firebaseConfig = {
   messagingSenderId: "296768918645",
   appId: "1:296768918645:web:3af5a398e8eee5fc0a943c"
 };
+
+//先定義出“應用程式”
+// 前端反映到後端,,定義ＡＰＰ＝firebase的ＡＰＰ啟動器（ 置入消防氣設定 ）
+const firebaseApp = firebase.initializeApp(firebaseConfig)
+
+//數據庫訪問透過= firebaseAPP  . firebase 
+const db = firebaseApp.firebase();
+const auth = firebase.auth();
+
+//New auth from google povider 
+const provider = new firebase.auth.GoogleAuthProvider();
+
+//設定到這等於完成啟動器-- open tour firebase website =>cloud store 
+
+export { auth, provider }
+//導出認證和估格的認證
+
+
+export default db;
+//export db 導出數據庫
+
